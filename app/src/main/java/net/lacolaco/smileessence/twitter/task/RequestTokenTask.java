52ffe6c041ca40c497bd454a8204e61c43619db9
Kazuki Yamaguchi <k@rhe.jp>
2015-09-24 02:58:44 +0900

    リクエストトークン取得時にはコールバック URL として oob を指定しておきたい

--- app/src/main/java/net/lacolaco/smileessence/twitter/task/RequestTokenTask.java
+++ app/src/main/java/net/lacolaco/smileessence/twitter/task/RequestTokenTask.java
@@ -24,7 +24,6 @@
 
 package net.lacolaco.smileessence.twitter.task;
 
-import net.lacolaco.smileessence.logging.Logger;
 import net.lacolaco.smileessence.util.BackgroundTask;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
@@ -44,6 +43,6 @@ public class RequestTokenTask extends BackgroundTask<RequestToken, Void> {
 
     @Override
     protected RequestToken doInBackground() throws TwitterException {
-        return twitter.getOAuthRequestToken();
+        return twitter.getOAuthRequestToken("oob");
     }
 }
